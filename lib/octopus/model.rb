f74dbc3f913f24a5ab04ef9107bd7a214e493b0a
Eric Harvey <eharvey2@gmail.com>
2019-03-05 17:16:57 -0800

    Removed using pls don't break

--- lib/octopus/model.rb
+++ lib/octopus/model.rb
@@ -9,21 +9,6 @@ module Octopus
     end
 
     module SharedMethods
-      def using(shard)
-        if block_given?
-          raise Octopus::Exception, <<-EOF
-#{name}.using is not allowed to receive a block, it works just like a regular scope.
-
-If you are trying to scope everything to a specific shard, use Octopus.using instead.
-          EOF
-        end
-
-        if Octopus.enabled?
-          Octopus::ScopeProxy.new(shard, self)
-        else
-          self
-        end
-      end
     end
 
     module InstanceMethods
